75a690099e871f08002dc69662f3b3338e5b1882
Shamika <redkars@sheridancollege.ca>
2023-08-25 10:51:42 -0400

    Configured firebase in the HabitrosApp file

--- Habitros/HabitrosApp.swift
+++ Habitros/HabitrosApp.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct HabitrosApp: App {
     @StateObject var viewModel = AuthViewModel()
+    
+    init(){
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
